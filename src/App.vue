6b1a32c89ac82619682433886f54216fac4f8213
tsapliya <s.ermishev@icloud.com>
2021-11-17 19:52:37 +0300

    перееезд на vue3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <home/>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .app {
+    width: 100vw;
+    height: 100vh;
+
+  }
+}
+
+
+</style>
+<script>
+import Home from "./views/Home";
+
+export default {
+  components: {Home}
+}
+</script>
